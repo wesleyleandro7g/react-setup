f8a82e844094f1bcddb3a6696fd751dea04ab695
wesleyleandro7g <wesleyleandro.dev@gmail.com>
2021-03-15 15:40:44 -0300

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,23 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Hello Title</title>
+      </Head>
+
+      <header>
+        Hello Header
+      </header>
+
+      <main>
+        <h1>Hello Word</h1>
+      </main>
+
+      <footer>
+        Hello Footer
+      </footer>
+    </div>
+  )
+}
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Setup para projetos React criados utilizando o "create next-app"
+
+### **1 - Intale o Typescript como dependencia de desenvolvimento**
+
+    yarn add typescript @types/react @types/node -D
+
+### **2 - Execute o projeto para que seja criado o arquivo "tsconfig"**
+
+### **3 -**
